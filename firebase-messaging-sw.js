c516adfcc4457f9ed72bb4436862f4dd4dba8506
ManLeMich <ti-mimi97115@hotmail.fr>
2026-03-18 20:21:28 +0100

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,29 @@
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD9EarYlpDbDfPL3n7AXS0Dv8U1tyTWeR8",
+  authDomain: "application-break-antillais.firebaseapp.com",
+  databaseURL: "https://application-break-antillais-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "application-break-antillais",
+  storageBucket: "application-break-antillais.firebasestorage.app",
+  messagingSenderId: "788821898426",
+  appId: "1:788821898426:web:992bc58a2f829028407e83"
+});
+
+const messaging = firebase.messaging();
+
+// Handle background notifications
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Background message:', payload);
+  const notif = payload.notification || {};
+  const title = notif.title || 'Break Antillais 🌴';
+  const options = {
+    body: notif.body || '',
+    icon: '/icon-192.png',
+    badge: '/icon-192.png',
+    vibrate: [300, 100, 300],
+    data: payload.data
+  };
+  return self.registration.showNotification(title, options);
+});
